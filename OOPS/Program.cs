31240077550cfbed680741fe7b64179943b4b718
chirag.chauhan <chirag.chauhan@promptdairytech.in>
2025-01-06 18:59:23 +0530

    Refactor namespaces and add new classes
    Replaced `System.Runtime.CompilerServices` with `System.Text`. Introduced `OOPS` and `MyNamespace` namespaces. Added `program` class in `OOPS` namespace. Created instances of `Maths`, `Social`, and `Science` in `Main` method. Changed `Social` class from static to regular. Added `MyClass` with properties and a private field. Added `Property_Function` class to demonstrate properties vs functions. Included comments explaining properties, functions, and namespaces.

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Text;
+
+
 
 namespace OOPS { 
 public class program
@@ -6,6 +8,7 @@ public class program
     public static void Main(string[] args)
     {
       // can not create object of abstract class , static class and private class.
+      
         Maths mats = new Maths();
         Social soc = new Social();
         Science scci = new Science();
@@ -28,3 +31,39 @@ public static class  Social
     
 }
 
+//Property is a class member that provides a flexible mechanism to read and write the value of a private filed.
+public class MyClass
+{
+    private int myProperty;
+    public int Myproperty
+    {
+        get { return myProperty; }
+        set{ myProperty = value; }
+    }
+    public string MyProperty1 { get; set; }
+}
+
+//what is the difference between Property and Function?
+
+//Property is a specialized function only
+//Specialized means properties are used only to get and set field values.
+public class Property_Function 
+{
+    public int MyProperty { get; set; }
+    public int MyProperty()
+    {
+        //property is a function only so we can not create duplicate property and function in same class.
+    }
+}
+
+//What are Namespaces?
+// A namespace is a container for a set of related classes and other types.
+//using System.Text;
+
+namespace MyNamespace
+{
+    public class MyClass
+    {
+        StringBuilder StringBuilder = new StringBuilder();
+    }
+}
