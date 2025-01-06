cd42058a953cb0d42c95c1979332c94e41b6dcb5
chirag.chauhan <chirag.chauhan@promptdairytech.in>
2025-01-06 18:29:14 +0530

    first commit

---
+++ OOPS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OOPS { 
+public class program
+{
+    public static void Main(string[] args)
+    {
+      // can not create object of abstract class , static class and private class.
+        Maths mats = new Maths();
+        Social soc = new Social();
+        Science scci = new Science();
+    }
+  
+}
+
+    private class Science
+    {
+
+    }
+}
+public abstract class Maths
+{
+
+}
+
+public static class  Social
+{
+    
+}
+
